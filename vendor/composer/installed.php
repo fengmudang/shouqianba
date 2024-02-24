4303647c42b5f5aef63d2e553b1695556b179200
liuxg <lxg@7cong.cn>
2024-02-25 03:38:23 +0800

    ?

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a1361b046a907acd174099563e7fe8651abcbebc',
+    'name' => 'liuxg/shouqianba',
+  ),
+  'versions' => 
+  array (
+    'liuxg/shouqianba' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a1361b046a907acd174099563e7fe8651abcbebc',
+    ),
+  ),
+);
